99b6bdf3d93c2d07e01b3227049a519eec52d96f
Rensutsuk <johnrenzoerfelp@gmail.com>
2023-06-01 14:32:14 +0800

    changed connection string

--- Includes/dbcon.php
+++ Includes/dbcon.php
@@ -1,11 +1,15 @@
 <?php
-	$host = "localhost";
-	$user = "root";
-	$pass = "";
-	$db = "attendance-management-db";
-	
-	$conn = new mysqli($host, $user, $pass, $db);
-	if($conn->connect_error){
-		echo "Seems like you have not configured the database. Failed To Connect to database:" . $conn->connect_error;
+	try {
+		$conn = new PDO("sqlsrv:server = tcp:eskolar.database.windows.net,1433; Database = eSkolar-AMS", "Rensutsuki", "%4iJQg4@KKe8pVG&");
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
+	catch (PDOException $e) {
+		print("Error connecting to SQL Server.");
+		die(print_r($e));
+	}
+	
+	// SQL Server Extension Sample Code:
+	$connectionInfo = array("UID" => "Rensutsuki", "pwd" => "%4iJQg4@KKe8pVG&", "Database" => "eSkolar-AMS", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+	$serverName = "tcp:eskolar.database.windows.net,1433";
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
