749c901dbf8b337b160326577711c89aac1a7a67
Rensutsuk <johnrenzoerfelp@gmail.com>
2023-06-01 14:40:16 +0800

    changed connection strings

--- Includes/dbcon.php
+++ Includes/dbcon.php
@@ -1,15 +1,16 @@
 <?php
-	try {
-		$conn = new PDO("sqlsrv:server = tcp:eskolar.database.windows.net,1433; Database = eSkolar-AMS", "Rensutsuki", "%4iJQg4@KKe8pVG&");
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	catch (PDOException $e) {
-		print("Error connecting to SQL Server.");
-		die(print_r($e));
+	$serverName = "tcp:eskolar.database.windows.net,1433";
+	$connectionOptions = array(
+		"Database" => "eSkolar-AMS",
+		"Uid" => "Rensutsuki",
+		"PWD" => "%4iJQg4@KKe8pVG&"
+	);
+	
+	$conn = sqlsrv_connect($serverName, $connectionOptions);
+	
+	if ($conn === false) {
+		die(print_r(sqlsrv_errors(), true));
 	}
 	
-	// SQL Server Extension Sample Code:
-	$connectionInfo = array("UID" => "Rensutsuki", "pwd" => "%4iJQg4@KKe8pVG&", "Database" => "eSkolar-AMS", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-	$serverName = "tcp:eskolar.database.windows.net,1433";
-	$conn = sqlsrv_connect($serverName, $connectionInfo);
+	// Perform database operations using $conn
 ?>
