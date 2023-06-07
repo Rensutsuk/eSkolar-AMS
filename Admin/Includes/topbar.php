3d2d114a23970aba0b131b45998acee893ab1bfe
Rensutsu <johnrenzoerfelp@gmail.com>
2023-06-08 02:44:02 +0800

    test

--- Admin/Includes/topbar.php
+++ Admin/Includes/topbar.php
@@ -6,47 +6,45 @@ $rows = $rs->fetch_assoc();
 $fullName = $rows['firstName'] . " " . $rows['lastName'];
 
 ?>
+
 <nav class="navbar navbar-expand bg-body-tertiary navbar-light bg-gradient-primary topbar mb-4 static-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-      <img src="img\logo\attnlg.png" alt="Bootstrap" width="45" height="45">
-      <ul class="nav nav-pills">
+  <div class="container-fluid">
+    <!-- Logo -->
+    <a class="navbar-brand nav-link text-white" href="index.php">
+      <img src="img\logo\attnlg.png" width="40" height="40" class="d-inline-block align-text-center">
+      eSkolar AMS
+    </a>
+    <!-- Navbar -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Active</a>
+          <a class="nav-link" href="createStudents.php">Students</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link" href="createClassTeacher.php">Teachers</a>
         </li>
       </ul>
-    </a>
-  </div>
-
-  <div class="topbar-divider d-none d-sm-block"></div>
-  <li class="nav-item dropdown no-arrow">
-    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-      aria-haspopup="true" aria-expanded="false">
-      <img class="img-profile rounded-circle" src="img/user-icn.png" style="max-width: 60px">
-      <span class="ml-2 d-none d-lg-inline text-white small"><b>Welcome
-          <?php echo $fullName; ?>
-        </b></span>
-    </a>
-    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-      <a class="dropdown-item" href="#">
-        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-        Profile
-      </a>
-      <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="logout.php">
-        <i class="fas fa-power-off fa-fw mr-2 text-danger"></i>
-        Logout
-      </a>
     </div>
-  </li>
-  </ul>
+    <!-- UserProfile Button -->
+    <div class="dropdown no-arrow open">
+      <button class="btn dropdown-toggle" type="button" id="triggerId" data-toggle="dropdown" aria-haspopup="true"
+        aria-expanded="false">
+        <img class="img-profile rounded-circle" src="img/user-icn.png" style="max-width: 40px">
+        <span class="ml-2 d-none d-lg-inline text-white small"><b>Welcome
+            <?php echo $fullName; ?>
+          </b></span>
+      </button>
+      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="triggerId">
+        <a class="dropdown-item" href="#">
+          <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+          Profile
+        </a>
+        <a class="dropdown-item" href="logout.php">
+          <i class="fas fa-power-off fa-fw mr-2 text-danger"></i>
+          Logout
+        </a>
+      </div>
+    </div>
+    </li>
+  </div>
 </nav>
--- Admin/index.php
+++ Admin/index.php
@@ -43,10 +43,6 @@ $rrw = $rs->fetch_assoc();
         <div class="container-fluid" id="container-wrapper">
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Administrator Dashboard</h1>
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="./">Home</a></li>
-              <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
-            </ol>
           </div>
 
           <div class="row mb-3">
