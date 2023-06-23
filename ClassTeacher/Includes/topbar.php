c7445619c40e21cf4286f7b1530e9eb64fb3be2e
Rensutsuk <johnrenzoerfelp@gmail.com>
2023-06-23 13:50:13 +0800

    commit

--- ClassTeacher/Includes/topbar.php
+++ ClassTeacher/Includes/topbar.php
@@ -1,67 +1,60 @@
+<?php
 
-<?php 
-
-  $query = "SELECT * FROM tblclassteacher WHERE Id = ".$_SESSION['userId']."";
-  $rs = $conn->query($query);
-  $num = $rs->num_rows;
-  $rows = $rs->fetch_assoc();
-  $fullName = $rows['firstName']." ".$rows['lastName'];;
+$query = "SELECT * FROM tblclassteacher WHERE Id = " . $_SESSION['userId'] . "";
+$rs = $conn->query($query);
+$num = $rs->num_rows;
+$rows = $rs->fetch_assoc();
+$fullName = $rows['firstName'] . " " . $rows['lastName'];
+;
 
 ?>
-<nav class="navbar navbar-expand navbar-light bg-gradient-primary topbar mb-4 static-top">
-          <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-          </button>
-        <div class="text-white big" style="margin-left:100px;"></div>
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-search fa-fw"></i>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                aria-labelledby="searchDropdown">
-                <form class="navbar-search">
-                  <div class="input-group">
-                    <input type="text" class="form-control bg-light border-1 small" placeholder="What do you want to look for?"
-                      aria-label="Search" aria-describedby="basic-addon2" style="border-color: #3f51b5;">
-                    <div class="input-group-append">
-                      <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i>
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </li>
-         
-           
-            <div class="topbar-divider d-none d-sm-block"></div>
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                <img class="img-profile rounded-circle" src="img/user-icn.png" style="max-width: 60px">
-                <span class="ml-2 d-none d-lg-inline text-white small"><b>Welcome <?php echo $fullName;?></b></span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <!-- <a class="dropdown-item" href="#">
-                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Profile
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Settings
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Activity Log
-                </a> -->
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="logout.php">
-                <i class="fas fa-power-off fa-fw mr-2 text-danger"></i>
-                  Logout
-                </a>
-              </div>
-            </li>
+<nav class="navbar navbar-expand bg-body-tertiary navbar-light bg-gradient-primary topbar mb-4 static-top">
+  <div class="container-fluid">
+    <!-- Logo -->
+    <a class="navbar-brand nav-link text-white" href="index.php">
+      <img src="img\logo\attnlg.png" width="40" height="40" class="d-inline-block align-text-center">
+      eSkolar AMS
+    </a>
+    <!-- Navbar -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-white" href="viewStudents.php">Students</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Attendance
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="viewAttendance.php">Class Attendance</a></li>
+            <li><a class="dropdown-item" href="viewStudentAttendance.php">Student Attendance</a></li>
+            <li><a class="dropdown-item" href="takeAttendance.php">Take Attendance</a></li>
+            <li><a class="dropdown-item" href="downloadRecord.php">Today's Report (xls)</a></li>
           </ul>
-        </nav>
+        </li>
+      </ul>
+    </div>
+    <!-- UserProfile Button -->
+    <div class="dropdown no-arrow open">
+      <button class="btn dropdown-toggle" type="button" id="triggerId" data-toggle="dropdown" aria-haspopup="true"
+        aria-expanded="false">
+        <img class="img-profile rounded-circle" src="img/user-icn.png" style="max-width: 40px">
+        <span class="ml-2 d-none d-lg-inline text-white small"><b>Welcome
+            <?php echo $fullName; ?>
+          </b></span>
+      </button>
+      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="triggerId">
+        <a class="dropdown-item" href="#">
+          <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+          Profile
+        </a>
+        <a class="dropdown-item" href="logout.php">
+          <i class="fas fa-power-off fa-fw mr-2 text-danger"></i>
+          Logout
+        </a>
+      </div>
+    </div>
+    </li>
+  </div>
+</nav>
