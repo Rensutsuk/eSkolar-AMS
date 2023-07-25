1563a1bce2e4d99374e758e85a1aedafacbcccac
Rensutsuk <96723635+Rensutsuk@users.noreply.github.com>
2023-07-26 00:00:26 +0800

    Revert "for editing take attendance"
    This reverts commit 521a0498aaaa8d9d08f22de728134019286026d5.

--- ClassTeacher/takeAttendance.php
+++ ClassTeacher/takeAttendance.php
@@ -7,56 +7,59 @@ $query = "SELECT tblclass.className,tblclassarms.classArmName
     FROM tblclassteacher
     INNER JOIN tblclass ON tblclass.Id = tblclassteacher.classId
     INNER JOIN tblclassarms ON tblclassarms.Id = tblclassteacher.classArmId
-    Where tblclassteacher.Id = '$_SESSION[userId]'";
+    WHERE tblclassteacher.Id = '$_SESSION[userId]'";
 $rs = $conn->query($query);
 $num = $rs->num_rows;
 $rrw = $rs->fetch_assoc();
 
-
 //session and Term
-$querey = mysqli_query($conn, "select * from tblsessionterm where isActive ='1'");
+$querey = mysqli_query($conn, "SELECT * FROM tblsessionterm WHERE isActive ='1'");
 $rwws = mysqli_fetch_array($querey);
 $sessionTermId = $rwws['Id'];
 
 $dateTaken = date("Y-m-d");
 
-$qurty = mysqli_query($conn, "select * from tblattendance  where classId = '$_SESSION[classId]' and classArmId = '$_SESSION[classArmId]' and dateTimeTaken='$dateTaken'");
+// Check if the attendance has been taken
+$qurty = mysqli_query($conn, "SELECT * FROM tblattendance WHERE classId = '$_SESSION[classId]' AND classArmId = '$_SESSION[classArmId]' AND dateTimeTaken='$dateTaken'");
 $count = mysqli_num_rows($qurty);
 
-if ($count == 0) { //if Record does not exsit, insert the new record
+// Variable to keep track if the table should be shown or hidden
+$tableHidden = $count > 0 ? 'd-none' : '';
 
-  //insert the students record into the attendance table on page load
-  $qus = mysqli_query($conn, "select * from tblstudents  where classId = '$_SESSION[classId]' and classArmId = '$_SESSION[classArmId]'");
+// If attendance has not been taken, insert the new record for each student
+if ($count == 0) {
+  $qus = mysqli_query($conn, "SELECT * FROM tblstudents  WHERE classId = '$_SESSION[classId]' AND classArmId = '$_SESSION[classArmId]'");
   while ($ros = $qus->fetch_assoc()) {
-    $qquery = mysqli_query($conn, "insert into tblattendance(admissionNo,classId,classArmId,sessionTermId,status,dateTimeTaken) 
-              value('$ros[admissionNumber]','$_SESSION[classId]','$_SESSION[classArmId]','$sessionTermId','0','$dateTaken')");
+    $studentAdmissionNumber = $ros['admissionNumber'];
+    $existingRecordQuery = mysqli_query($conn, "SELECT * FROM tblattendance WHERE admissionNo = '$studentAdmissionNumber' AND dateTimeTaken = '$dateTaken'");
 
+    if (mysqli_num_rows($existingRecordQuery) == 0) {
+      $qquery = mysqli_query($conn, "INSERT INTO tblattendance (admissionNo, classId, classArmId, sessionTermId, status, dateTimeTaken) 
+              VALUES ('$studentAdmissionNumber','$_SESSION[classId]','$_SESSION[classArmId]','$sessionTermId','0','$dateTaken')");
+    }
   }
 }
 
 if (isset($_POST['save'])) {
-
   $admissionNo = $_POST['admissionNo'];
-
   $check = $_POST['check'];
   $N = count($admissionNo);
   $status = "";
 
-  //check if the attendance has not been taken i.e if no record has a status of 1
-  $qurty = mysqli_query($conn, "select * from tblattendance  where classId = '$_SESSION[classId]' and classArmId = '$_SESSION[classArmId]' and dateTimeTaken='$dateTaken' and status = '1'");
+  // Check if the attendance has been taken
+  $qurty = mysqli_query($conn, "SELECT * FROM tblattendance  WHERE classId = '$_SESSION[classId]' AND classArmId = '$_SESSION[classArmId]' AND dateTimeTaken='$dateTaken' AND status = '1'");
   $count = mysqli_num_rows($qurty);
 
   if ($count > 0) {
     $statusMsg = "<div class='alert alert-danger' style='margin-right:700px;'>Attendance has been taken for today!</div>";
-
   } else {
     for ($i = 0; $i < $N; $i++) {
-      $admissionNo[$i]; //admission Number
+      $admissionNumber = $admissionNo[$i];
 
       if (isset($check[$i])) {
-        $qquery = mysqli_query($conn, "update tblattendance set status='1' where admissionNo = '$check[$i]'");
+        $qquery = mysqli_query($conn, "UPDATE tblattendance SET status='1' WHERE admissionNo = '$admissionNumber'");
         if ($qquery) {
-          $statusMsg = "<div class='alert alert-success'  style='margin-right:700px;'>Attendance Taken Successfully!</div>";
+          $statusMsg = "<div class='alert alert-success' style='margin-right:700px;'>Attendance Taken Successfully!</div>";
         } else {
           $statusMsg = "<div class='alert alert-danger' style='margin-right:700px;'>An error Occurred!</div>";
         }
@@ -80,8 +83,6 @@ if (isset($_POST['save'])) {
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
   <link href="css/ruang-admin.min.css" rel="stylesheet">
-
-
 
   <script>
     function classArmDropdown(str) {
@@ -118,20 +119,22 @@ if (isset($_POST['save'])) {
         <div class="container-fluid" id="container-wrapper">
           <div class="row">
             <div class="col-lg-12">
+              <!-- Input Group -->
               <form method="post">
                 <div class="row">
                   <div class="col-lg-12">
                     <div class="card mb-4">
                       <div
                         class="card-header bg-navbar py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h1 class="h5 mb-0 text-primary">Take Attendance (Today's Date :
+                        <h1 class="h5 mb-0 text-primary">Attendance of Class
+                          <?php echo $rrw['className'] . ' - ' . $rrw['classArmName']; ?>
+                          (
                           <?php echo $todaysDate = date("m-d-Y"); ?>)
                         </h1>
-
-                        <h1 class="h6 mb-0 text-warning"><i>Click on the checkboxes besides each student to take
-                            attendance!</i></h1>
                       </div>
-                      <div class="table-responsive p-3">
+                      <h6 class="m-3 font-weight-bold text-danger">Note: <i>Click on the checkboxes besides each
+                          student to take attendance!</i></h6>
+                      <div class="table-responsive p-3 <?php echo $tableHidden; ?>">
                         <?php echo $statusMsg; ?>
                         <table class="table align-items-center table-flush table-hover">
                           <thead class="thead-light">
@@ -139,7 +142,7 @@ if (isset($_POST['save'])) {
                               <th>#</th>
                               <th>First Name</th>
                               <th>Last Name</th>
-                              <th>Middle Name</th>
+                              <th>Other Name</th>
                               <th>Admission No</th>
                               <th>Class</th>
                               <th>Class Arm</th>
@@ -151,11 +154,11 @@ if (isset($_POST['save'])) {
 
                             <?php
                             $query = "SELECT tblstudents.Id,tblstudents.admissionNumber,tblclass.className,tblclass.Id As classId,tblclassarms.classArmName,tblclassarms.Id AS classArmId,tblstudents.firstName,
-                                      tblstudents.lastName,tblstudents.otherName,tblstudents.admissionNumber,tblstudents.dateCreated
-                                      FROM tblstudents
-                                      INNER JOIN tblclass ON tblclass.Id = tblstudents.classId
-                                      INNER JOIN tblclassarms ON tblclassarms.Id = tblstudents.classArmId
-                                      where tblstudents.classId = '$_SESSION[classId]' and tblstudents.classArmId = '$_SESSION[classArmId]'";
+                      tblstudents.lastName,tblstudents.otherName,tblstudents.admissionNumber,tblstudents.dateCreated
+                      FROM tblstudents
+                      INNER JOIN tblclass ON tblclass.Id = tblstudents.classId
+                      INNER JOIN tblclassarms ON tblclassarms.Id = tblstudents.classArmId
+                      where tblstudents.classId = '$_SESSION[classId]' and tblstudents.classArmId = '$_SESSION[classArmId]'";
                             $rs = $conn->query($query);
                             $num = $rs->num_rows;
                             $sn = 0;
@@ -164,16 +167,16 @@ if (isset($_POST['save'])) {
                               while ($rows = $rs->fetch_assoc()) {
                                 $sn = $sn + 1;
                                 echo "
-                                  <tr>
-                                    <td>" . $sn . "</td>
-                                    <td>" . $rows['firstName'] . "</td>
-                                    <td>" . $rows['lastName'] . "</td>
-                                    <td>" . $rows['otherName'] . "</td>
-                                    <td>" . $rows['admissionNumber'] . "</td>
-                                    <td>" . $rows['className'] . "</td>
-                                    <td>" . $rows['classArmName'] . "</td>
-                                    <td><input name='check[]' type='checkbox' value=" . $rows['admissionNumber'] . " class='form-control'></td>
-                                  </tr>";
+                              <tr>
+                                <td>" . $sn . "</td>
+                                <td>" . $rows['firstName'] . "</td>
+                                <td>" . $rows['lastName'] . "</td>
+                                <td>" . $rows['otherName'] . "</td>
+                                <td>" . $rows['admissionNumber'] . "</td>
+                                <td>" . $rows['className'] . "</td>
+                                <td>" . $rows['classArmName'] . "</td>
+                                <td><input name='check[]' type='checkbox' value=" . $rows['admissionNumber'] . " class='form-control'></td>
+                              </tr>";
                                 echo "<input name='admissionNo[]' value=" . $rows['admissionNumber'] . " type='hidden' class='form-control'>";
                               }
                             } else {
@@ -182,23 +185,26 @@ if (isset($_POST['save'])) {
                             No Record Found!
                             </div>";
                             }
+
                             ?>
                           </tbody>
                         </table>
                         <br>
                         <button type="submit" name="save" class="btn btn-primary">Take Attendance</button>
+                        <!-- Display an alert if attendance is already taken -->
               </form>
             </div>
           </div>
         </div>
       </div>
+      <div class="alert alert-danger mt-4 <?php echo !$tableHidden ? 'd-none' : ''; ?>" role="alert">
+        Attendance has been taken for today!
+      </div>
     </div>
-  </div>
+    <!---Container Fluid-->
   </div>
   <!-- Footer -->
   <?php include "Includes/footer.php"; ?>
-  </div>
-  </div>
 
   <!-- Scroll to top -->
   <a class="scroll-to-top rounded" href="#page-top">
